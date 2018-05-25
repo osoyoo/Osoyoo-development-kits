9735ac11db147a70190a83b2bc23e07605ff840c
osoyoo <yujian_ca@hotmail.com>
2018-05-25 10:34:47 +0800

    libraries for yun iot kit
    libraries for yun iot kit

--- Osoyoo IoT Kit/Cayenne_BMP180_Example/Cayenne_BMP180_Example.ino
+++ Osoyoo IoT Kit/Cayenne_BMP180_Example/Cayenne_BMP180_Example.ino
@@ -30,9 +30,9 @@ Steps:
 #include <CayenneMQTTEthernet.h>
 
 // Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
-char username[] = "put your username here ";
-char password[] = "put your password here";
-char clientID[] = "put your clientID here";
+char username[] = "c4cb3ce0-4029-11e7-98bd-9ffdab1de3b9";
+char password[] = "086740b8d0b9882b1191342fa0984c795cba7ca7";
+char clientID[] = "a51c3620-4ea5-11e8-aaba-83a3e9c1fc99";
 
 #define TEMPERATURE_VIRTUAL_CHANNEL 5
 #define BAROMETER_VIRTUAL_CHANNEL 6
