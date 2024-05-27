b8d9a21e3ae2e3481ec7b07d4500f1644b4a40d7
osoyoo <yujian_ca@hotmail.com>
2024-05-27 18:07:57 +0800

    Update IR_basic.ino

--- Osoyoo lessons for Arduino/IR_basic/IR_basic.ino
+++ Osoyoo lessons for Arduino/IR_basic/IR_basic.ino
@@ -1,23 +1,15 @@
-#include <IRremote.h>  // use the IRRemote.h
-const int irReceiverPin = 2;  //the SIG of receiver module attach to pin2
-IRrecv irrecv(irReceiverPin); //Creates a variable of type IRrecv
-decode_results results;    // define resultsas 
+#include "IRremote.hpp"
 
-void setup()
-{
-  Serial.begin(9600);    //initialize serial,baudrate is 9600
-  irrecv.enableIRIn();   // enable ir receiver module
+#define IR_RECEIVE_PIN 2
+
+void setup() {
+  Serial.begin(9600);
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);  
 }
 
-void loop() 
-{
-  if (irrecv.decode(&results)) //if the ir receiver module receiver data
-  {  
-    Serial.print("irCode: ");    //print "irCode: "        
-    Serial.print(results.value, HEX); //print the value in hexdecimal 
-    Serial.print(",  bits: ");  //print" , bits: "         
-    Serial.println(results.bits); //print the bits
-    irrecv.resume();    // Receive the next value 
-  }  
-  delay(600); //delay 600ms
+void loop() {
+  if (IrReceiver.decode()) {
+    IrReceiver.printIRResultShort(&Serial);
+    IrReceiver.resume();
+  }
 }
